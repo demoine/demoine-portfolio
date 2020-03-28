6c9339d70a2eaafbd16bfec954edaca6e61b1bc1
“demoine” <demoine00@gmail.com>
2020-03-28 00:14:14 -0400

    commit

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'London',
-  shortName: 'London',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Imed Adel',
-  twitter: 'Imed_Adel'
+  name: "DeMoine Lovelace",
+  shortName: "DeMoine",
+  description: "Designer with Front End Programming Skills.",
+  url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "DeMoine Lovelace",
+  twitter: "DeMoine Lovelace",
 }
