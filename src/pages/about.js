91d55223149b63c4551aca02b34198d7af5b9164
“demoine” <demoine00@gmail.com>
2020-08-15 23:31:03 -0400

    logo test

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,8 @@ import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { ReactComponent as XDLogo } from "../../content/assets/icons/Adobe_XD_CC_icon.svg"
+
 import "../utils/normalize.css"
 import "../utils/css/screen.css"
 
@@ -46,7 +48,7 @@ const AboutPage = ({ data }, location) => {
                     background: "#eee",
                   }}
                 >
-                  Adobe XD
+                  <ReactLogo />
                 </div>
               </div>
               <div className="col">
