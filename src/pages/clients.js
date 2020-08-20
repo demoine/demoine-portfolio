19bcf32f3c4fe7f83494632f2f3bf14f942ad42e
“demoine” <demoine00@gmail.com>
2020-08-20 19:04:37 -0400

    client logos

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -5,6 +5,22 @@ import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Project01 from "../../content/assets/icons/charleston.png"
+import Project02 from "../../content/assets/icons/unlv.png"
+import Project03 from "../../content/assets/icons/cocacola.png"
+import Project04 from "../../content/assets/icons/crowneplaza.png"
+import Project05 from "../../content/assets/icons/dayton.png"
+import Project06 from "../../content/assets/icons/durex.png"
+import Project07 from "../../content/assets/icons/mit.png"
+import Project08 from "../../content/assets/icons/mlb.png"
+import Project09 from "../../content/assets/icons/msu.png"
+import Project10 from "../../content/assets/icons/ogilvy.png"
+import Project11 from "../../content/assets/icons/spanx.png"
+import Project12 from "../../content/assets/icons/takeda.png"
+import Project13 from "../../content/assets/icons/twf.png"
+import Project14 from "../../content/assets/icons/ua.png"
+import Project15 from "../../content/assets/icons/zoosk.png"
+
 import "../utils/normalize.css"
 import "../utils/css/screen.css"
 
@@ -34,7 +50,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project01} alt="Project Name" />
               </div>
             </div>
             <div className="col">
@@ -45,7 +61,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project02} alt="Project Name" />
               </div>
             </div>
             <div className="col">
@@ -56,7 +72,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project03} alt="Project Name" />
               </div>
             </div>
             <div className="col">
@@ -67,54 +83,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col">
-              <div
-                style={{
-                  padding: "1rem 0",
-                  textAlign: "center",
-                  background: "#eee",
-                }}
-              >
-                auto
-              </div>
-            </div>
-            <div className="col">
-              <div
-                style={{
-                  padding: "1rem 0",
-                  textAlign: "center",
-                  background: "#eee",
-                }}
-              >
-                auto
-              </div>
-            </div>
-            <div className="col">
-              <div
-                style={{
-                  padding: "1rem 0",
-                  textAlign: "center",
-                  background: "#eee",
-                }}
-              >
-                auto
-              </div>
-            </div>
-            <div className="col">
-              <div
-                style={{
-                  padding: "1rem 0",
-                  textAlign: "center",
-                  background: "#eee",
-                }}
-              >
-                auto
+                <img src={Project04} alt="Project Name" />
               </div>
             </div>
           </div>
@@ -128,7 +97,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project05} alt="Project Name" />
               </div>
             </div>
             <div className="col">
@@ -139,7 +108,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project06} alt="Project Name" />
               </div>
             </div>
             <div className="col">
@@ -150,7 +119,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project07} alt="Project Name" />
               </div>
             </div>
             <div className="col">
@@ -161,7 +130,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project08} alt="Project Name" />
               </div>
             </div>
           </div>
@@ -175,7 +144,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project09} alt="Project Name" />
               </div>
             </div>
             <div className="col">
@@ -186,7 +155,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project10} alt="Project Name" />
               </div>
             </div>
             <div className="col">
@@ -197,7 +166,7 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project11} alt="Project Name" />
               </div>
             </div>
             <div className="col">
@@ -208,8 +177,53 @@ const ElementsPage = ({ data }, location) => {
                   background: "#eee",
                 }}
               >
-                auto
+                <img src={Project12} alt="Project Name" />
               </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col">
+              <div
+                style={{
+                  padding: "1rem 0",
+                  textAlign: "center",
+                  background: "#eee",
+                }}
+              >
+                <img src={Project13} alt="Project Name" />
+              </div>
+            </div>
+            <div className="col">
+              <div
+                style={{
+                  padding: "1rem 0",
+                  textAlign: "center",
+                  background: "#eee",
+                }}
+              >
+                <img src={Project14} alt="Project Name" />
+              </div>
+            </div>
+            <div className="col">
+              <div
+                style={{
+                  padding: "1rem 0",
+                  textAlign: "center",
+                  background: "#eee",
+                }}
+              >
+                <img src={Project15} alt="Project Name" />
+              </div>
+            </div>
+            <div className="col">
+              <div
+                style={{
+                  padding: "1rem 0",
+                  textAlign: "center",
+                  background: "#eee",
+                }}
+              ></div>
             </div>
           </div>
         </div>
